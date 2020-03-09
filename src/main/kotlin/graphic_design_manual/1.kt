a5f4de533f3bc0048eb5bd59b65dcbc57bceb382
Rein van der Woerd <reinvanderwoerd@me.com>
2020-03-09 09:40:13 +0100

    Update 1.kt

--- src/main/kotlin/graphic_design_manual/1.kt
+++ src/main/kotlin/graphic_design_manual/1.kt
@@ -2,6 +2,8 @@ package graphic_design_manual
 
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.math.Vector2
+import kotlin.random.Random
 
 
 fun main() = application {
@@ -11,8 +13,22 @@ fun main() = application {
     }
 
     program {
+        var positions: List<Vector2> = listOf()
+        var random = Random(0)
+
+        for (i in 0..50) {
+            positions += Vector2(random.nextInt(50).toDouble(), random.nextInt(50).toDouble())
+        }
+
         extend {
             drawLines(this)
+
+            // White blocks
+            drawer.stroke = ColorRGBa.WHITE
+            for (p in positions) {
+                drawer.rectangle(p.x / 50 * width - width / 100, p.y / 50 * width - width / 100,
+                            width / 50.0, height / 50.0)
+            }
         }
     }
 }
