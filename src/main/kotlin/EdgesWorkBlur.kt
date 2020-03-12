f8387802b826c5dbe4fa695901adefabd1a68673
Rein van der Woerd <reinvanderwoerd@me.com>
2020-03-12 18:45:06 +0100

    Continued with booklet...

--- src/main/kotlin/EdgesWorkBlur.kt
+++ src/main/kotlin/EdgesWorkBlur.kt
@@ -3,11 +3,13 @@ import org.openrndr.application
 import org.openrndr.draw.isolatedWithTarget
 import org.openrndr.draw.loadImage
 import org.openrndr.draw.renderTarget
+import org.openrndr.extensions.Screenshots
 import org.openrndr.extra.compositor.compose
 import org.openrndr.extra.compositor.draw
 import org.openrndr.extra.compositor.layer
 import org.openrndr.extra.compositor.post
 import org.openrndr.extra.compositor.compose
+import org.openrndr.extra.fx.antialias.FXAA
 import org.openrndr.extra.fx.color.ChromaticAberration
 import org.openrndr.extra.fx.edges.EdgesWork
 import org.openrndr.math.clamp
@@ -16,9 +18,11 @@ import org.openrndr.math.map
 
 fun main() = application {
     configure {
-        width = 1400
-        height = 1400
-        fullscreen = Fullscreen.CURRENT_DISPLAY_MODE
+//        width = 1400
+        width = 108 * 4
+        height = 175 * 4
+//        height = 1400
+//        fullscreen = Fullscreen.CURRENT_DISPLAY_MODE
     }
 
     program {
@@ -40,10 +44,12 @@ fun main() = application {
                 }
 
                 post(ew)
+                post(FXAA())
             }
         }
 
 //        extend(ScreenRecorder())
+        extend(Screenshots())
         extend {
             // Mouse means radius
             ew.radius = clamp(map(0.0, width.toDouble(), 10.0, 800.0, mouse.position.x), 10.0, 400.0).toInt()
