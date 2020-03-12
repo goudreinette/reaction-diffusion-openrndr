bbcd3b857fdbe3d90978d7279cbafeeb786f7a3b
Rein van der Woerd <reinvanderwoerd@me.com>
2020-03-12 23:08:07 +0100

    A cross!

--- src/main/kotlin/vector_control/VectorPanelControl.kt
+++ src/main/kotlin/vector_control/VectorPanelControl.kt
@@ -7,11 +7,11 @@ import org.openrndr.draw.Drawer
 import org.openrndr.draw.FontImageMap
 import org.openrndr.panel.ControlManager
 import org.openrndr.panel.elements.*
-import org.openrndr.panel.style.LinearDimension
-import org.openrndr.panel.style.fontFamily
-import org.openrndr.panel.style.fontSize
+import org.openrndr.panel.style.*
 import org.openrndr.shape.Rectangle
+import org.openrndr.shape.contour
 import org.openrndr.text.Writer
+import kotlin.and
 import kotlin.math.round
 
 class VectorControl : Element(ElementType("vector")) {
@@ -48,54 +48,23 @@ class VectorControl : Element(ElementType("vector")) {
     }
 
     override val widthHint: Double
-        get() {
-            computedStyle.let { style ->
-                val fontUrl = (root() as? Body)?.controlManager?.fontManager?.resolve(style.fontFamily) ?: "broken"
-                val fontSize = (style.fontSize as? LinearDimension.PX)?.value ?: 14.0
-                val fontMap = FontImageMap.fromUrl(fontUrl, fontSize)
+        get() = 200.0
 
-                val writer = Writer(null)
-
-                writer.box = Rectangle(0.0,
-                    0.0,
-                    Double.POSITIVE_INFINITY,
-                    Double.POSITIVE_INFINITY)
-
-                writer.drawStyle.fontMap = fontMap
-                writer.newLine()
-                writer.text(label, visible = false)
-
-                return writer.cursor.x
-            }
-        }
 
      override fun draw(drawer: Drawer) {
-
         computedStyle.let {
-
             drawer.pushTransforms()
             drawer.pushStyle()
-            drawer.fill = ColorRGBa.PINK
+            drawer.fill = ColorRGBa.GRAY
             drawer.stroke = null
             drawer.strokeWeight = 0.0
 
             drawer.rectangle(0.0, 0.0, layout.screenWidth, layout.screenHeight)
 
-            (root() as? Body)?.controlManager?.fontManager?.let {
-                val font = it.font(computedStyle)
-                val writer = Writer(drawer)
-                drawer.fontMap = (font)
-                val textWidth = writer.textWidth(label)
-                val textHeight = font.ascenderLength
-
-                val offset = round((layout.screenWidth - textWidth) / 2.0)
-                val yOffset = round((layout.screenHeight / 2) + textHeight / 2.0 - 2.0) * 1.0
-
-                drawer.fill =  ColorRGBa.WHITE.opacify(
-                    if (disabled in pseudoClasses) 0.25 else 1.0
-                )
-                drawer.text(label, 0.0 + offset, 0.0 + yOffset)
-            }
+            drawer.stroke = ColorRGBa.GRAY.shade(1.4)
+            drawer.strokeWeight = 1.0
+            drawer.lineSegment(0.0, layout.screenHeight / 2.0, layout.screenWidth, layout.screenHeight / 2.0)
+            drawer.lineSegment(layout.screenWidth / 2.0, 0.0, layout.screenWidth / 2.0, layout.screenHeight)
 
             drawer.popStyle()
             drawer.popTransforms()
@@ -113,10 +82,37 @@ fun main() = application {
 
     program {
         val cm = ControlManager()
-        var bgColor = ColorRGBa.PINK
+        var bgColor = ColorRGBa.BLACK
+
+        // TODO: move to default stylesheet
+        cm.layouter.styleSheets.add(styleSheet(has type "vector") {
+            display = Display.BLOCK
+            background = Color.RGBa(ColorRGBa.GRAY)
+            width = LinearDimension.Auto
+            height = 96.px
+            paddingLeft = 10.px
+            paddingRight = 10.px
+            marginLeft = 5.px
+            marginRight = 5.px
+            marginTop = 5.px
+            marginBottom = 5.px
+
+//            and(has state "selected") {
+//                display = Display.BLOCK
+//                background = controlActiveColor
+//            }
+            and(has state "hover") {
+                display = Display.BLOCK
+                background  = Color.RGBa(ColorRGBa.GRAY.shade(1.5))
+            }
+        })
 
         cm.body = layout(cm) {
             initElement(arrayOf(), VectorControl()) {
+
+            }
+
+            button {
 
             }
         }
